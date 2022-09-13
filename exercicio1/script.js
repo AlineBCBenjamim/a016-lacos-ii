1c852093846d7aa4ec185b8e23b3f06a2531b964
Aline Carvalho <alineoak9@gmail.com>
2022-09-13 20:01:34 -0300

    Resolução dos exercicios A016

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,12 @@
-const array = [
-    [27, 4, 20, 13, 14],
-    [11, 15, 12, 8, 9],
-    [5, 5, 12, 16, 4],
-    [20, 33, 11, 12, 19],
-    [3, 3, 4, 5, 10]
+/* Um analista quer conseguir ver quantos gols cada um de seus 5 atacantes marcou nas últimas 5 temporadas, e quer que você construa um código que mostre isso.*/
+
+const ultimasTemporadas = [
+    [27, 4, 20, 13, 14], // posição 0
+    [11, 15, 12, 8, 9],  // posição 1
+    [5, 5, 12, 16, 4],   // posição 2
+    [20, 33, 11, 12, 19], // posição 3
+    [3, 3, 4, 5, 10]      // posição 4
 ]
+for (let i = 0; i < ultimasTemporadas.length; i++){
+    console.log(`Jogador ${i+1} : ${array[i]}`);
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,7 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
+let i = 1
+for(rankingPaises of maioresPaises){
+    console.log(`Ranking dos países ${i}º ${rankingPaises}`);
+    i++
+} 
+
